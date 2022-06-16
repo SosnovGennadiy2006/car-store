8dacc45ffac08b53d469059da016dceb0ddac874
Дмитрий Середин <dark.skinermen@gmail.com>
2022-06-16 21:31:31 +0300

    Add detail class

--- src/logics/products/detail.cpp
+++ src/logics/products/detail.cpp
@@ -1,6 +1,119 @@
+//
+// Created by skine on 6/6/2022.
+//
+
 #include "detail.h"
 
-Detail::Detail()
-{
+detail::detail() {
+    nameManufacturer = "";
+    engineVolume = -1;
+    enginePower = -1;
+    wheelRubber = undefined_rubberType;
+    detailType = undefined_detailType;
+    candleType = undefined_candleType;
+    carburetorType = undefined_carburetorType;
+    caliperType = undefined_caliperType;
+    brakeDiscsType = undefined_brakeDiscsType;
+    brakeDiscsTypeOfMaterial = undefined_brakeDiscsTypeOfMaterial;
+    heatNumber = -1;
+    wheelRubberRadius = -1;
+    wheelDiskRadius = -1;
+}
 
+std::string detail::getNameManufacturer() const{
+    return nameManufacturer;
+}
+void detail::setNameManufacturer(std::string new_val){
+    nameManufacturer = new_val;
+}
+
+double detail::getEngineVolume() const{
+    return engineVolume;
+}
+void detail::setEngineVolume(double new_val) {
+    engineVolume = new_val;
+}
+
+int detail::getEnginePower() const {
+    return enginePower;
+}
+void detail::setEnginePower(int new_val) {
+    enginePower = new_val;
+}
+
+RubberTypes detail::getWheelRubber() const{
+    return wheelRubber;
+}
+void detail::setWheelRubber(RubberTypes new_val) {
+    wheelRubber = new_val;
+}
+
+DetailTypes detail::getDetailType() const {
+    return detailType;
+}
+void detail::setDetailType(DetailTypes new_val){
+    detailType = new_val;
+}
+
+int detail::getCandleSize() const{
+    return candleSize;
+}
+void detail::setCandleSize(int new_val){
+    candleSize = new_val;
+}
+
+CandleTypes detail::getCandleType() const{
+    return candleType;
+}
+void detail::setCandleType(CandleTypes new_val){
+    candleType = new_val;
+}
+
+CarburetorTypes detail::getCarburetorType() const{
+    return carburetorType;
+}
+void detail::setCarburetorType(CarburetorTypes new_val){
+    carburetorType = new_val;
+}
+
+CaliperType detail::getCaliperType() const{
+    return caliperType;
+}
+void detail::setCaliperType(CaliperType new_val){
+    caliperType = new_val;
+}
+
+BrakeDiscsTypes detail::getBrakeDiscsType() const{
+    return brakeDiscsType;
+}
+void detail::setBrakeDiscsType(BrakeDiscsTypes new_val){
+    brakeDiscsType = new_val;
+}
+
+BrakeDiscsTypesOfMaterial detail::getBrakeDiscsTypeOfMaterial() const{
+    return brakeDiscsTypeOfMaterial;
+}
+void detail::setBrakeDiscsTypeOfMaterial(BrakeDiscsTypesOfMaterial new_val){
+    brakeDiscsTypeOfMaterial = new_val;
+}
+
+int detail::getHeatNumber() const{
+    return heatNumber;
+}
+void detail::setHeatNumber(int new_val){
+    heatNumber = new_val;
+}
+
+int detail::getWheelRubberRadius() const{
+    return wheelRubberRadius;
+}
+void detail::setWheelRubberRadius(int new_val){
+    wheelRubberRadius = new_val;
+}
+
+int detail::getWheelDiskRadius() const{
+    return wheelDiskRadius;
+}
+void detail::setWheelDiskRadius(int new_val){
+    wheelDiskRadius = new_val;
 }
--- src/logics/products/detail.h
+++ src/logics/products/detail.h
@@ -1,11 +1,132 @@
-#ifndef DETAIL_H
-#define DETAIL_H
+//
+// Created by skine on 6/6/2022.
+//
 
+#ifndef CARSTORE_DETAIL_H
+#define CARSTORE_DETAIL_H
+#include <string>
 
-class Detail
-{
-public:
-    Detail();
+//Типы деталей
+enum DetailTypes{
+    detail_engine,
+    detail_wheelRubber,
+    detail_wheelDisk,
+    detail_candle,
+    detail_carburetor,
+    detail_caliper,
+    detail_brakeDiscs,
+    undefined_detailType
 };
 
-#endif // DETAIL_H
+//Типы резины
+enum RubberTypes{
+    winter_tires,
+    summer_tires,
+    undefined_rubberType
+};
+
+//Типы свечей
+enum CandleTypes{
+    candle_singleElectrode,
+    candle_multielectrode,
+    candle_special,
+    candle_torch,
+    candle_plasmaPrechamber,
+    undefined_candleType
+};
+
+//Типы карбюратора
+enum CarburetorTypes{
+    carburetor_bubbling, //Барботажный
+    carburetor_wick, //Фитильный
+    carburetor_membraneNeedle, //Мембранно-игольчатый
+    carburetor_float, //Поплавковый
+    undefined_carburetorType
+};
+
+//Виды суппортов
+enum CaliperType{
+    caliper_floating, //плавающий
+    caliper_fixed, //фиксированный
+    undefined_caliperType
+};
+
+//Виды тормозных дисков
+enum BrakeDiscsTypes{
+    brakeDiscs_solid, //Сплошные
+    brakeDiscs_ventilated, //Вентилируемые
+    brakeDiscs_composite, //Составные
+    undefined_brakeDiscsType
+};
+
+//Виды тормозных дисков о материалу
+enum BrakeDiscsTypesOfMaterial{
+    brakeDiscsMat_metal, //Металлические
+    brakeDiscsMat_ceramic, //Керамические
+    undefined_brakeDiscsTypeOfMaterial
+};
+
+class detail {
+    std::string nameManufacturer;
+    double engineVolume;
+    int enginePower;
+    RubberTypes wheelRubber;
+    DetailTypes detailType;
+    int candleSize;
+    CandleTypes candleType;
+    CarburetorTypes carburetorType;
+    CaliperType caliperType;
+    BrakeDiscsTypes brakeDiscsType;
+    BrakeDiscsTypesOfMaterial brakeDiscsTypeOfMaterial;
+    int heatNumber;
+    int wheelRubberRadius;
+    int wheelDiskRadius;
+
+public:
+    detail();
+
+    std::string getNameManufacturer() const;
+    void setNameManufacturer(std::string new_val);
+
+    double getEngineVolume() const;
+    void setEngineVolume(double new_val);
+
+    int getEnginePower() const;
+    void setEnginePower(int new_val);
+
+    RubberTypes getWheelRubber() const;
+    void setWheelRubber(RubberTypes new_val);
+
+    DetailTypes getDetailType() const;
+    void setDetailType(DetailTypes new_val);
+
+    int getCandleSize() const;
+    void setCandleSize(int new_val);
+
+    CandleTypes getCandleType() const;
+    void setCandleType(CandleTypes new_val);
+
+    CarburetorTypes getCarburetorType() const;
+    void setCarburetorType(CarburetorTypes new_val);
+
+    CaliperType getCaliperType() const;
+    void setCaliperType(CaliperType new_val);
+
+    BrakeDiscsTypes getBrakeDiscsType() const;
+    void setBrakeDiscsType(BrakeDiscsTypes new_val);
+
+    BrakeDiscsTypesOfMaterial getBrakeDiscsTypeOfMaterial() const;
+    void setBrakeDiscsTypeOfMaterial(BrakeDiscsTypesOfMaterial new_val);
+
+    int getHeatNumber() const;
+    void setHeatNumber(int new_val);
+
+    int getWheelRubberRadius() const;
+    void setWheelRubberRadius(int new_val);
+
+    int getWheelDiskRadius() const;
+    void setWheelDiskRadius(int new_val);
+};
+
+
+#endif //CARSTORE_DETAIL_H
